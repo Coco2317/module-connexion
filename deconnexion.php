7d60385c122b36911dba06256431bb2e78398139
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-08-06 12:04:50 +0200

    setting deconnexion

--- deconnexion.php
+++ deconnexion.php
@@ -1,22 +1,6 @@
 <?php
 session_start();
-
-// Stocker le message dans une variable temporaire
-$message = "Vous avez été déconnecté.";
-
-// Détruire la session actuelle
-$_SESSION = [];
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
-}
+session_unset();
 session_destroy();
-
-// Démarrer une nouvelle session pour stocker le message
-session_start();
-$_SESSION['message'] = $message;
-session_write_close();
-
-header("Location: index.php");
+header("Location: connexion.php");
 exit;
