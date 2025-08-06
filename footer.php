caa41838d0ee0bcbc82fbc37713c13a6aae503a3
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-08-06 12:05:09 +0200

    footer commun

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </main>
-<footer>
-    <p>&copy; 2025 - Projet module de connexion</p>
+<footer style="text-align:center; padding:20px; background:#f1f1f1; margin-top:40px;">
+    &copy; <?= date('Y') ?> Team Front End 🤘🏽. Tous droits réservés.
 </footer>
 </body>
 </html>
