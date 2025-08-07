65a458b99c0b4c4b018ad00670a729aee29ed083
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-08-07 10:28:18 +0200

    configuration

--- config.php
+++ config.php
@@ -1,10 +1,22 @@
 <?php
+$secure = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off');
+
+session_set_cookie_params([
+    'lifetime' => 7200,
+    'path' => '/',
+    'secure' => $secure,
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
+
+session_start();
+
 $servername = "localhost";
-$username = "root"; 
-$password = "";   
+$username = "root";
+$password = "";
 $dbname = "moduleconnexion";
 
-$conn = new mysqli($servername, $username, $password, $dbname); /*nouvelle connexion base de données*/
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connexion échouée : " . $conn->connect_error);
